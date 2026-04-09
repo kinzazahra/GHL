de73fd5a6440b45066d3586dc204f97578d74b8e
Kinza Zahra <yourgithubemail@example.com>
2026-04-09 22:51:41 +0530

    updated templates with base.html and fixed pages

--- app.py
+++ app.py
@@ -3,9 +3,25 @@ import os
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
+# HOME PAGE
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# ABOUT PAGE
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# SERVICES PAGE
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+# INVESTORS PAGE
+@app.route('/investors')
+def investors():
+    return render_template('investors.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
